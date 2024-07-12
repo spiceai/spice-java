14741c12df5bf4abea0450d23d919df225700244
sgrebnov <sergei.grebnov@gmail.com>
2024-07-11 17:28:03 -0700

    Update Debug configuration for VScode

--- src/main/java/ai/spice/example/ExampleSpiceCloudPlatform.java
+++ src/main/java/ai/spice/example/ExampleSpiceCloudPlatform.java
@@ -28,7 +28,7 @@ import ai.spice.SpiceClient;
 
 /**
  * Example of using Spice.ai client to query data from Spice.ai Cloud
- * mvn exec:java -Dexec.mainClass="ai.spice.example.Example"
+ * mvn exec:java -Dexec.mainClass="ai.spice.example.ExampleSpiceCloudPlatform"
  */
 public class ExampleSpiceCloudPlatform {
 
