23eee97bde1998555c03a18e0f58904af71bc23c
sgrebnov <sergei.grebnov@gmail.com>
2024-08-19 11:46:44 -0700

    Accelerated dataset refresh method and example

---
+++ src/main/java/ai/spice/example/ExampleDatasetRefreshSpiceOSS.java
@@ -0,0 +1,63 @@
+/*
+Copyright 2024 The Spice.ai OSS Authors
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package ai.spice.example;
+
+import java.net.URI;
+
+import org.apache.arrow.flight.FlightStream;
+import org.apache.arrow.vector.VectorSchemaRoot;
+
+import ai.spice.SpiceClient;
+
+/**
+ * Example of using SDK with Spice.ai OSS (Local)
+ * _JAVA_OPTIONS="--add-opens=java.base/java.nio=ALL-UNNAMED" mvn exec:java -Dexec.mainClass="ai.spice.example.ExampleDatasetRefreshSpiceOSS"
+ * 
+ * Requires local Spice OSS running. Follow the quickstart
+ * https://github.com/spiceai/spiceai?tab=readme-ov-file#%EF%B8%8F-quickstart-local-machine.
+ */
+public class ExampleDatasetRefreshSpiceOSS {
+
+    public static void main(String[] args) {
+        try (SpiceClient client = SpiceClient.builder()
+                .withFlightAddress(URI.create("http://localhost:50051"))
+                .withHttpAddress(URI.create("http://localhost:8090"))
+                .build()) {
+
+            client.refresh("taxi_trips");
+            System.out.println("Dataset refresh triggered for taxi_trips");
+
+            System.out.println("Query taxi_trips dataset");
+            FlightStream stream = client.query("SELECT * FROM taxi_trips LIMIT 1;");
+
+            while (stream.next()) {
+                try (VectorSchemaRoot batches = stream.getRoot()) {
+                    System.out.println(batches.contentToTSVString());
+                }
+            }
+
+        } catch (Exception e) {
+            System.err.println("An unexpected error occurred: " + e.getMessage());
+        }
+    }
+}
